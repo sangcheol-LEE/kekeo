83743f5551f4c73201c29084682f80e73916dd60
이상철 <zlrz6706@gmail.com>
2022-11-13 23:01:15 +0900

    스키마 작성 완료

---
+++ packages/server/src/schemas/chat.ts
@@ -0,0 +1,28 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../sequelize";
+import User from "./user";
+import Room from "./room";
+
+
+const Chat = sequelize.define("chat", {
+  id: {
+    type : DataTypes.INTEGER,
+    primaryKey : true,
+    autoIncrement: true
+  },
+  content: DataTypes.STRING,
+  senderId : {
+    type : DataTypes.UUID,
+    references : {
+      model : User,
+    }
+  },
+  roomId : {
+    type: DataTypes.INTEGER,
+    references : {
+      model : Room,
+    }
+  }
+})
+
+export default Chat;
---
+++ packages/server/src/server.ts
@@ -0,0 +1,8 @@
+import express, { Application } from "express";
+
+const app: Application = express();
+
+
+app.listen(8000, () => {
+  console.log("start")
+})
