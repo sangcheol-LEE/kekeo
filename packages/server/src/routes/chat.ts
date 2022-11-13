d526756432cae0ff491d4b14667e3dbe831e5c25
이상철 <zlrz6706@gmail.com>
2022-11-13 23:14:18 +0900

    채팅방 라우팅 작업

---
+++ packages/server/src/routes/chat.ts
@@ -0,0 +1,40 @@
+import { request, response, Router } from "express";
+import Chat from "../schemas/chat";
+import User from "../schemas/user";
+import Room from "../schemas/room";
+
+
+
+
+const router = Router();
+
+/* 채팅 목록 */
+router.get("/:roomId", async(request, response) => {
+  try {
+    const chat = await Chat.findAll({
+      where: {
+        roomId: request.params.roomId
+      },
+      include: [User, Room]
+    });
+
+    response.json(chat)
+  } catch(e){}
+})
+
+/* 채팅 전송 */
+router.post("/:roomId", async (request, response) => {
+  try {
+    const chat = await Chat.create({
+      senderId : request.session.userId,
+      content: request.body.content,
+      roomId: request.params.roomId
+    });
+
+    /* TODO:: socket */
+
+    response.json({ message: "OK!"});
+  } catch(e) {}
+})
+
+export default router
