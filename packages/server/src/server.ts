095f53cb10aa7d2f03229b14fc7cf29392723b1b
이상철 <zlrz6706@gmail.com>
2022-11-14 23:03:53 +0900

    session 설정 완료

--- packages/server/src/server.ts
+++ packages/server/src/server.ts
@@ -1,8 +1,22 @@
 import express, { Application } from "express";
+import session from "express-session";
+
 import sequelize from "./sequelize";
-import routes from "./routes"
+import routes from "./routes";
+
+const FileStore = require("session-file-store")(session);
+
 const app: Application = express();
 
+const sessionMiddleware = session({
+  secret: "kekeonibs", // 쿠키를 임의로 변조하는 것을 방지하기 위한 값으로써 이 값을 통해 세션을 암호화하여 저장함
+  saveUninitialized: true, // 세션이 저장되기 전에 언 이니셜라이즈드 상태로 만들어서 저장하겠다라는 설정
+  cookie: { secure: false },
+  resave: false, // 세션을 언제나 저장할지 정하는 값
+  store: new FileStore()
+})
+
+app.use(sessionMiddleware)
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
 
